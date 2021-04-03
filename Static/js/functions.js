fcadb49e82ae272b6364ceaafc8e1b8b674c80e9
Harvey Cameron <2467610c@student.gla.ac.uk>
2021-04-03 16:56:37 +0100

    requirements plus making scripts and css external

---
+++ Static/js/functions.js
@@ -0,0 +1,10 @@
+function alertFunction(){
+        alert("Do you really want to log out?");
+    }
+function copyText() {
+		var copyText = document.getElementById("CopyCode");
+		copyText.select();
+		copyText.setSelectionRange(0, 99999)
+		document.execCommand("copy");
+		alert("Promo " + copyText.value + " copied!");
+	}
