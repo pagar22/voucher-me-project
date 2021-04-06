9b97b29289c97109fa2d6a452b33cdfd32e83c5f
charlieyunyun <80698575+charlieyunyun@users.noreply.github.com>
2021-04-06 09:10:30 +0800

    Update views.py

--- voucherMe/views.py
+++ voucherMe/views.py
@@ -1,11 +1,11 @@
 # AARYAN
-from datetime import datetime
-
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from datetime import datetime
 from django.urls import reverse
-
+from django.contrib.auth import login
 from voucherMe.forms import BusinessForm, PostForm
 from voucherMe.models import UserProfile, Business, Post
 
@@ -33,11 +33,12 @@ def profile(request, username):
     context_dict = {'user': user, 'userprofile': userprofile, 'businesses': businesses}
     return render(request, 'voucher/account.html', context=context_dict)
 
+
 def search(request, type):
     context_dict = {}
     if request.method == 'GET':
         query = request.GET.get('search')
-        query.strip() #attempt to remove whitespaces
+        query.strip()  # attempt to remove whitespaces
         try:
             if type == "business":
                 result = Business.objects.filter(name__icontains=query).order_by('-likes')
@@ -49,7 +50,6 @@ def search(request, type):
         context_dict['results'] = result
         return render(request, 'voucher/search.html', context=context_dict)
     return render(request, 'voucher/search.html', context=context_dict)
-
 
 
 # Add
@@ -110,14 +110,15 @@ def all_businesses(request):
     context_dict = {'businesses': business_list}
     return render(request, 'voucher/all_businesses.html', context=context_dict)
 
+
 def all_posts(request):
     posts_list = Post.objects.order_by('-visits')
-    context_dict = {'posts':posts_list}
+    context_dict = {'posts': posts_list}
     return render(request, 'voucher/all_posts.html', context=context_dict)
+
 
 def show_business(request, business_name_slug):
     context_dict = {}
-    posts = []
     try:
         business = Business.objects.get(slug=business_name_slug)
         posts = Post.objects.filter(business_id=business).order_by('-visits')
@@ -129,25 +130,6 @@ def show_business(request, business_name_slug):
         context_dict['business'] = None
         context_dict['posts'] = None
         context_dict['user'] = None
-
-    total_visits = 0
-    popularity = 0
-    if posts:
-        for post in posts:
-            total_visits = total_visits+post.visits
-
-    if 0 <= total_visits <= 10:
-        popularity = 1
-    elif 10 < total_visits <= 20:
-        popularity = 2
-    elif 20 < total_visits <= 40:
-        popularity = 3
-    elif 40 < total_visits <= 70:
-        popularity = 4
-    elif total_visits > 70:
-        popularity = 5
-
-    context_dict['popularity'] = popularity
     return render(request, 'voucher/business.html', context=context_dict)
 
 
@@ -156,17 +138,26 @@ def show_post(request, business_name_slug, post_id):
     try:
         business = Business.objects.get(slug=business_name_slug)
         post = Post.objects.get(id=post_id)
+        vistor_cookie_handler(request)
+        post.visits = request.session['visits']
         context_dict['business'] = business
         context_dict['post'] = post
+        context_dict['visits'] = post.visits
+        context_dict['promo'] = post.promo
     except Post.DoesNotExist:
         context_dict['business'] = None
         context_dict['post'] = None
-
-    visitor_cookie_handler(request)
-    post.visits = post.visits+request.session['visits']
-    post.save()
     return render(request, 'voucher/post.html', context=context_dict)
 
+
+def BusinessLike(request, pk):
+    post = get_object_or_404(Business, id=request.POST.get('add_like'))
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+    else:
+        post.likes.add(request.user)
+
+    return redirect(reverse('voucher/business.html', args=[str(pk)]))
 
 
 # cookie handler
@@ -177,7 +168,7 @@ def get_server_side_cookie(request, cookie, default_val=None):
     return val
 
 
-def visitor_cookie_handler(request):
+def vistor_cookie_handler(request):
     visits = int(get_server_side_cookie(request, 'visits', 1))
     last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
     last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
@@ -189,3 +180,29 @@ def visitor_cookie_handler(request):
     request.session['visits'] = visits
 
 
+def add_data(request):
+    user_obj = User.objects.all().last()
+    k = user_obj.id
+
+    for uu in range(k, 100 + k):
+        User.objects.create(username='test{}'.format(uu),
+                            password='aiyouwoqu',
+                            email='123{}@qq.com'.format(uu))
+
+        Business.objects.create(name='test{}'.format(uu),
+                                description='test1',
+                                slug='123test{}'.format(uu),
+                                image='media/profile_images/IMG_20180428_112912_769.jpg',
+                                user_id=uu)
+        business_obj = Business.objects.all().last()
+        kk = business_obj.id
+        Post.objects.create(name='test{}'.format(uu),
+                            description='this is a test',
+                            tags_category='test{}'.format(uu),
+                            tags_type='1',
+                            promo='test{}'.format(uu),
+                            business_id=kk,
+                            visits=11
+                            )
+
+    return HttpResponse('data add sucess')
